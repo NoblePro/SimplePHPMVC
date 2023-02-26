c3876cf5dabdfad901c03482b52ded6c262f0140
NoblePro <126382577+NoblePro@users.noreply.github.com>
2023-02-26 16:53:12 +0800

    Delete pengaduan directory

--- pengaduan/app/models/Home_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Home_model{
-	public function getName($id){
-		$query = 'SELECT * FROM `user` WHERE id="'.$id.'"';
-		$dbcontroller = new DBController();
-		$this->memory = $dbcontroller->executeSelectQuery($query);
-		return $this->memory;
-	}
-}
--- pengaduan/app/models/Login_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Login_model{
-	public function checkLogin($username, $password){
-		$query = 'SELECT * FROM `user` WHERE username="'.$username.'"';
-		$dbcontroller = new DBController();
-		$this->memory = $dbcontroller->executeSelectQuery($query);
-		return $this->memory;
-	}
-}
